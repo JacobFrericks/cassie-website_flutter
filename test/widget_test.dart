e4857baa9570b580f917ac47308a84ab30c691ad
Jacob Frericks <jacob.frericks@gmail.com>
2020-03-07 17:02:53 -0600

    Fixing tests
    The command "flutter test" returns an exit code 1 if no tests ran. I added a noop so they'll pass. That way all the scaffolding exists if I want to write a test in the future.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,5 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:midiowavending_flutter/main.dart';
 
 void main() {
-
+  testWidgets("noop", (WidgetTester tester) async {
+    expect(0, 0);
+  });
 }
